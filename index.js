f3ea5ed78c7c68e612f7f34dbbfe987a5f2b4ff4
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-07 15:30:07 +0500

    ..

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-           ///Task # 2 ///
+///Task # 2 ///
 //Personal Message: Store a person’s name in a variable, and print a message to 
 //that person. Your message should be simple, such as, “Hello Eric, would you 
 //like to learn some Python today?”
-
-let personName: string = "Eric,";
+let personName = "Eric,";
 console.log(`Hello ${personName} Would you like to learn some Python today?`);
+export {};
